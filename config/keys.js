72b9e3602c6eef7d630d8ef5d0dad1a2ff611f9d
Swift Pay Cash <paycashswift@gmail.com>
2022-08-27 08:08:08 +0100

    all

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+dbPassword22 = 'mongodb://localhost:27017/mern2';
+dbPassword= 'mongodb+srv://minters:minterspass@cluster0.tw2jw.mongodb.net/minters';
+module.exports = {
+    mongoURI: dbPassword
+};
